7620c1a76d2e9c501ddf75718cba22d1f8b4f0ac
fail2ban1337 <abelomar@student.1337.ma>
2022-03-06 12:49:57 +0100

    login page

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -5,8 +5,14 @@ import {
   StyleSheet,
   Image,
   ImageBackground,
+  TouchableNativeFeedback,
 } from "react-native";
+import * as SecureStore from "expo-secure-store";
+
+import * as Linking from "expo-linking";
+
 import { useFonts } from "expo-font";
+import * as WebBrowser from "expo-web-browser";
 
 import image42 from "../assets/42-white.png";
 
@@ -17,15 +23,30 @@ export default function LoginScreen() {
   if (!loaded) {
     return null;
   }
+  let url =
+    "https://api.intra.42.fr/oauth/authorize?client_id=897ad82467da0b23311fe619b9f9f0fb8f608bc98dc97a748ae73f00842acc97&redirect_uri=exp%3A%2F%2F172.20.10.14%3A19000&response_type=code";
+  const _handlePressButtonAsync = async () => {
+    WebBrowser.openBrowserAsync(url);
+    Linking.addEventListener("url", async (event) => {
+      let data = Linking.parse(event.url);
+      code = data.queryParams.code;
+      const token = await SecureStore.getItemAsync("secure_token");
+      if (!code || token) WebBrowser.dismissBrowser();
+      await SecureStore.setItemAsync("secure_token", code);
+      console.log(token);
+    });
+  };
   return (
     <ImageBackground
       source={require("../assets/back42.jpg")}
       style={styles.background}
     >
       <Image style={styles.logo} source={image42} />
-      <View style={styles.loginwrap}>
-        <Text style={styles.loginwrapText}>Login with 42</Text>
-      </View>
+      <TouchableNativeFeedback onPress={_handlePressButtonAsync}>
+        <View style={styles.loginwrap}>
+          <Text style={styles.loginwrapText}>Login with 42</Text>
+        </View>
+      </TouchableNativeFeedback>
     </ImageBackground>
   );
 }
